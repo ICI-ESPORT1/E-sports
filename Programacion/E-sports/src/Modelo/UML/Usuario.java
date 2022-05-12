31f2b6ac940f44dd2b67ad075a9ffc18f01e8a4b
celiagarcia1984 <94176982+celiagarcia1984@users.noreply.github.com>
2022-05-12 16:04:36 +0200

    Revert "Programacion celia"

---
+++ Programacion/E-sports/src/Modelo/UML/Usuario.java
@@ -0,0 +1,27 @@
+package Modelo.UML;
+
+import java.util.ArrayList;
+
+/**
+ * @Author Iker Gomez
+ * version 1
+ */
+public class Usuario {
+
+    public ArrayList<Jornada> VisualizarJornadas(int numJornada){
+        return null;
+    }
+
+    public ArrayList<Resultado> VisualizarClasificacion(){
+        return null;
+    }
+
+    public ArrayList<Resultado> VisualizarResutlados(){
+        return null;
+    }
+
+    public ArrayList<Resutlado> VisualizarResultados(String nombreEquipo){
+        return null;
+    }
+
+}
